b097802bf40cee0c2b905f1d3bce4888fe4ff458
alimalim77 <alimalim77@gmail.com>
2023-07-05 11:55:40 +0530

    Commiy

--- app.js
+++ app.js
@@ -12,46 +12,37 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// Mock user data (replace this with a database in a real application)
-const users = [
-  { username: "user1", password: "password1" },
-  { username: "user2", password: "password2" },
-  { username: "user3", password: "password3" },
-];
-
 // Route for user login
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
   console.log("Hello World");
 
   // Validate username format and length
-  const alphanumericRegex = /^[a-zA-Z0-9]+$/;
+  const isAlphanumeric = (str) => {
+    // Check if the string contains only alphanumeric characters
+    const alphanumericChars =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    for (let i = 0; i < str.length; i++) {
+      if (!alphanumericChars.includes(str[i])) {
+        return false;
+      }
+    }
+
+    return true;
+  };
   const validUsername =
-    alphanumericRegex.test(username) &&
-    username.length >= 6 &&
-    username.length <= 12;
+    alphanumericChars(str) && username.length >= 6 && username.length <= 12;
 
   // Validate password length
   const validPassword = password.length >= 6;
 
   if (validUsername && validPassword) {
-    // Check if the user exists
-    const user = users.find(
-      (u) => u.username === username && u.password === password
-    );
-
-    if (user) {
-      // Successful login
-      res.status(200).json({ message: "Login successful" });
-    } else {
-      // Invalid credentials
-      res.status(401).json({ error: "Invalid username or password" });
-    }
+    // Successful login
+    res.status(200).json({ message: "Login successful" });
   } else {
-    // Invalid username or password format/length
-    res
-      .status(400)
-      .json({ error: "Invalid username or password format/length" });
+    // Invalid credentials
+    res.status(401).json({ error: "Invalid username or password" });
   }
 });
 
